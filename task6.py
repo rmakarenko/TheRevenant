720eeae7e16a13fe0a3e244faf00e9ca29581a4f
rmakarenko <roman.makarenko.spb@gmail.com>
2021-10-11 15:16:54 +0300

    Update task6.py

--- task6.py
+++ task6.py
@@ -1,40 +1,54 @@
-class Coder:
+length = 0.00000
+matrix_code = [[6, 1, 9], [5, 2, 8], [4, 3, 7]]
+t1 = [0, 0]
+t2 = [0, 0]
 
-    length = 0
-    matrix_code = [[6, 1, 9], [5, 2, 8], [4, 3, 7]]
+def IncreaseLength(t1, t2):
 
-    def IncreaseLength(t1, t2):
-        if t1[0] == t2[0] or t1[1] == t2[1]:
-            Coder.length = Coder.length + 1
-        else:
-            Coder.length = Coder.length + 1.41421
+    global length
 
-    def GetCoordinates(digit):
-        for i in range(3):
-            for j in range(3):
-                if Coder.matrix_code[i][j] == digit:
-                    return i, j
+    if t1[0] == t2[0] or t1[1] == t2[1]:
+        length = length + 1
+    else:
+        length = length + 1.41421
 
-    def PatternUnlock(N, hits):
+def GetCoordinates(digit):
+    for i in range(3):
+        for j in range(3):
+            if matrix_code[i][j] == digit:
+                return i, j
 
-        t1 = [0, 0]
-        t2 = [0, 0]
+def PatternUnlock(N, hits):
 
-        for i in range(N - 1):
+    for i in range(N - 1):
+        t1 = GetCoordinates(hits[i])
+        t2 = GetCoordinates(hits[i + 1])
+        IncreaseLength(t1, t2)
 
-            t1 = Coder.GetCoordinates(hits[i])
+    outputLength = ''
+    stringLength = str(length)
+    integralPart = stringLength.split('.')[0]
+    fractionalPart = stringLength.split('.')[1]
+    digitsNumberintegralPart = len(str(integralPart))
+    digitsNumberfractionalPart = len(str(fractionalPart))
 
-            t2 = Coder.GetCoordinates(hits[i + 1])
+    for i in range(digitsNumberintegralPart): # уберем ноли из целой части
+        if integralPart[i] != 0:
+            outputLength = outputLength + integralPart[i]
 
-            Coder.IncreaseLength(t1, t2)
+    if digitsNumberfractionalPart <= 5:  # если дробная часть короче пятого знака то избавимся от нолей
+        for i in range(digitsNumberfractionalPart):
+            if fractionalPart[i] != 0:
+                outputLength = outputLength + fractionalPart[i]
 
-        outputLength = []
+    if digitsNumberfractionalPart > 5: # если дробная часть длиннее пятого знака то округлим ее и избавимся от нолей
+        for i in range(4): # обработаем дробную часть до предпоследнего знака вкючительно
+            if fractionalPart[i] != 0:
+                outputLength = outputLength + fractionalPart[i]
 
-        stringLength = str(Coder.length)
+        if int(fractionalPart[5]) > 5 and fractionalPart[4] != 9:         # последнююю цифру отдельно обработаем:
+            outputLength = outputLength + str(int(fractionalPart[4]) + 1)
+        elif int(fractionalPart[5]) <= 5 and fractionalPart[4] != 0:
+            outputLength = outputLength + str(fractionalPart[4])
 
-        for i in range(len(stringLength)):
-            if ((stringLength[i]) != '0'):
-
-                outputLength.append(stringLength[i])
-
-        return outputLength
+    return outputLength
