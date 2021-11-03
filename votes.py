ae3e8e9d585ea38867cafb17d1745e7e8848793b
rmakarenko <roman.makarenko.spb@gmail.com>
2021-11-03 22:10:37 +0300

    Update votes.py

--- votes.py
+++ votes.py
@@ -2,6 +2,7 @@ def Rounder3(number):  # в этой функции округлим до тре
 
     rounded = str(int(number)) + '.'
     fractional = str(number).split('.')[1]
+
     if len(fractional) > 3:
         for i in range(3):
             if i == 2 and int(fractional[3]) > 5:
@@ -10,23 +11,26 @@ def Rounder3(number):  # в этой функции округлим до тре
                 rounded = rounded + str(fractional[2])
             else:
                 rounded = rounded + str(fractional[i])
-    x =  float(rounded)
-    return int(x)
+
+    return float(rounded)
 
 def Max1Max2(Votes):
+    first = 0
+    second = 0
+    first_i = 0
+    second_i = 0
+
     for i in range(len(Votes)):  # проходим по массиву и сравниваем текущий элемент с максимумом и вторым числом
-        if (i == 0):  # при первом проходе заполним максимум и второй максимум первым элементом
-            max1 = Votes[i]
-            max2 = Votes[i]
-            max1i = i
-            max2i = i
-        elif (Votes[i] > max1):
-            max1 = Votes[i]
-            max1i = i
-        elif (Votes[i] > max2):
-            max2 = Votes[i]
-            max2i = i
-    return max1, max1i, max2, max2i
+        if (Votes[i] > first):
+            second = first
+            first = Votes[i]
+            second_i = first_i
+            first_i = i
+        elif (Votes[i] > second):
+            second = Votes[i]
+            second_i = i
+
+    return first, first_i, second, second_i
 
 def MassVote(N, Votes):
 
@@ -41,15 +45,10 @@ def MassVote(N, Votes):
     first_votes = Max1Max2(Votes)[0]
     second_votes = Max1Max2(Votes)[2]
     first_number = Max1Max2(Votes)[1]
-    second_number = Max1Max2(Votes)[3]
 
     # вычислим и округлим до третьего знака значения их процентов
-
     first_percents = first_votes * 100 / total_votes
     second_percents = second_votes * 100 / total_votes
-
-    rounded_first_percents = Rounder3(first_percents)
-    rounded_second_percents = Rounder3(second_percents)
 
     # проверим, какое из условий выполняется
 
