699299980c167c70ba9e739727ef5cb90708e04d
rmakarenko <roman.makarenko.spb@gmail.com>
2021-11-27 22:21:18 +0300

    Create bast_shoe.py

---
+++ bast_shoe.py
@@ -0,0 +1,58 @@
+current_string = ''
+current_position = 0
+current_state = []
+undo_done = 0
+
+def BastShoe(command):
+
+    global current_string
+    global current_position
+    global current_state
+    global undo_done
+
+    user_input = str(input('введите функцию 1-5 и параметр'))
+
+    if user_input.split(' ')[0] == '1':  # here in this block user input is handled and operations are performed
+        current_string = current_string + command.split(' ')[1]
+        current_position = current_position + 1
+        current_state.append(current_string)
+        undo_done = 0
+
+    elif user_input.split(' ')[0] == '2' and len(current_string) - int(user_input.split(' ')[1]) > 0:  # operation 2
+        buffered_string = current_string
+        current_string = ''
+        for i in range(len(current_string) - int(user_input.split(' ')[1])):
+            current_string = current_string + buffered_string[i]
+        current_position = current_position + 1
+        current_state.append(current_string)
+        undo_done = 0
+
+    elif user_input.split(' ')[0] == '2' and (len(current_string) - int(user_input.split(' ')[1]) <= 0):  # operation 2, returning empty string
+        undo_done = 0
+        return ''
+
+    elif user_input.split(' ')[0] == '3' and int(user_input.split(' ')[1]) < len(current_string) and int(user_input.split(' ')[1]) >= 0  :  # operation 3
+        return current_string[int(user_input.split(' ')[1])]
+
+    elif user_input.split(' ')[0] == '3' and (int(user_input.split(' ')[1]) < len(current_string) or int(user_input.split(' ')[1]) >= 0):  # operation 3
+        return ''
+
+    elif user_input.split(' ')[0] == '4' and current_position > 1:  # operation 4, undo
+        current_string = current_state[current_position - 1]
+        current_position = current_position - 1
+        undo_done = undo_done + 1
+
+    elif user_input.split(' ')[0] == '4' and current_position <= 1:  # operation 4, undo, when position is lower, than 1
+        current_position = current_position
+        current_string = ''
+        undo_done = undo_done + 1
+
+    elif user_input.split(' ')[0] == '5' and undo_done > 0:  # operation 5, redo
+        current_position = current_position + 1
+        current_string = current_state[current_position - 1]
+        undo_done = undo_done - 1
+
+    else:
+        return current_string
+
+    return current_string
