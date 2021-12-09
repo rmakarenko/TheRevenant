d574feb1a7a3e32e8bdf3ee299a4f9b92bc9fec1
rmakarenko <roman.makarenko.spb@gmail.com>
2021-12-09 20:58:52 +0300

    Update sherlock.py

--- sherlock.py
+++ sherlock.py
@@ -1,5 +1,4 @@
 def UniqueList(s):
-
     unique = [s[0]]
     for i in range(len(s)):  #  this block creates the list of the unique elements of the string
         for j in range(len(unique)):
@@ -8,44 +7,42 @@ def UniqueList(s):
     return unique
 
 def SherlockValidString(s):
-
     unique = UniqueList(s)
     unique_amount = []
     current_list = []
     delta_one = 0
-
+    delta = 0
     for i in range(len(unique)):  # put 0 for every unique element
         unique_amount.append(0)
-
     for i in range(len(s)):
         for j in range(len(unique)):
             if s[i] == unique[j]:
                 unique_amount[j] = unique_amount[j] + 1  # now we have the list with amounts of all unique elements
-
-    if len(unique_amount) == 1:
-        return True
-
-    if len(unique_amount) == 2 and unique_amount[0] - unique_amount[1] <= 1 and unique_amount[0] - unique_amount[1] >= -1:
-        return True
-    elif len(unique_amount) == 2 and unique_amount[0] - unique_amount[1] > 1 or unique_amount[0] - unique_amount[1] < -1:
-        return False
-
     unique_amount.sort(reverse=True)
-
-    unique_from_amounts = UniqueList(unique_amount)
-
-    for i in range(len(unique_from_amounts)):  # in this block we will get the lists with unique amounts
-        for j in range(len(unique_amount)):
-            if unique_from_amounts[i] == unique_amount[j]:
-                current_list.append(unique_from_amounts[i])
-
-    current_list.sort(reverse=True)
-
-    for i in range(len(current_list) - 2):
-        if current_list[i] - current_list[i + 1] > 1:
-            return False
-        elif current_list[i] - current_list[i + 1] == 1:
-            delta_one = delta_one + 1
-
-    if delta_one < 2:
+    if len(unique_amount) == 1:  # f len is 1 it is always true
         return True
+    if len(unique_amount) == 2 and unique_amount[0] == 1 or unique_amount[1] == 1:  # len = 2 and all equal
+        return True
+    if len(unique_amount) == 2 and unique_amount[0] - unique_amount[1] <= 1 and unique_amount[0] - unique_amount[1] >= -1:
+        return True  # len = 2 and delta less or equal than 1
+    elif len(unique_amount) == 2 and unique_amount[0] - unique_amount[1] > 1 or unique_amount[0] - unique_amount[1] < -1:
+        return False # len = 2 and delta more than 1
+    for i in range(len(unique_amount) - 2):
+        if unique_amount[i] != unique_amount[i + 1]:
+            delta = delta + 1
+    if delta == 0:
+        return True
+    if len(unique_amount) > 2 and unique_amount[len(unique_amount) - 1] == 1:  # all equal, one is 1
+        for i in range(len(unique_amount) - 2, 1, -1):
+            if unique_amount[i] != unique_amount[i - 1]:
+                return False
+            elif unique_amount[i] == unique_amount[i - 1] and i == 1:
+                return True
+    if len(unique_amount) > 2 and unique_amount[0] - unique_amount[1] == 1:  # all equal, one is more by 1
+        unique_amount[0] = unique_amount[0] - 1
+        for i in range(len(unique_amount) - 1):
+            if unique_amount[i] != unique_amount[i + 1]:
+                return False
+            elif unique_amount[i] == unique_amount[i + 1] and i == len(unique_amount) - 2:
+                return True
+    return False
