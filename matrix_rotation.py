10eacb3f454d8dbda59d38179779d162344d6e31
rmakarenko <roman.makarenko.spb@gmail.com>
2021-12-24 21:38:22 +0300

    Update matrix_rotation.py

--- matrix_rotation.py
+++ matrix_rotation.py
@@ -1,55 +1,35 @@
-def rotation(input_matrix, circuits):
+def MatrixTurn(data, h, w, t):
 
-    current_matrix = []
-    rotated_matrix = []
-    N = len(input_matrix[0])
-    M = len(input_matrix)
+    cycles = []
+    for l in range(min(h, w) // 2):
+        line = []
+        for i in range(l, w - l - 1):
+            line.append(data[l][i])
+        for i in range(l, h - l - 1):
+            line.append(data[i][-l - 1])
+        for i in range(w - l - 1, l, -1):
+            line.append(data[-l - 1][i])
+        for i in range(h - l - 1, l, -1):
+            line.append(data[i][l])
+        cycles.append(line)
 
-    for i in range(M):  # copy the input matrix to the current matrix and rotated matrix
-        current_list = []
-        for j in range(N):
-            current_list.append(input_matrix[i][j])
-        current_matrix.append(current_list)
-        rotated_matrix.append(current_list)
+    matrix = [[''] * w for x in range(h)]
+    for l in range(min(h, w) // 2):
+        c = - t
+        cycle_size = len(cycles[l])
+        for i in range(l, w - l - 1):
+            matrix[l][i] = cycles[l][c % cycle_size]
+            c += 1
+        for i in range(l, h - l - 1):
+            matrix[i][-l - 1] = cycles[l][c % cycle_size]
+            c += 1
+        for i in range(w - l - 1, l, -1):
+            matrix[-l - 1][i] = cycles[l][c % cycle_size]
+            c += 1
+        for i in range(h - l - 1, l, -1):
+            matrix[i][l] = cycles[l][c % cycle_size]
+            c += 1
 
-    for i in range(circuits):  # last curcuit will be handled in the next cycle
-
-        for j in range(N - 1 - i, i, - 1):  # upper horizontal line
-            rotated_matrix[i][j] = current_matrix[i][j - 1]
-
-        for j in range(i + 1, M - 2 * i):  # right vertical line
-            rotated_matrix[j][M - 1 - i] = current_matrix[j - 1][M - 1 - i]
-
-        for j in range(i, N - 1 - i):  # down horizontal line
-            rotated_matrix[M - 1 - i][j] = current_matrix[M - 1 - i][j + 1]
-
-        for j in range(i, M - 2 * i - 1):  # left vertical line
-            rotated_matrix[j][i] = current_matrix[j + 1][i]
-
-        for m in range(M):  # copy the rotated matrix to the current matrix in order to keep it actual
-            current_list = []
-            for n in range(N):
-                current_list.append(rotated_matrix[m][n])
-            current_matrix.append(current_list)
-
-    return rotated_matrix
-
-def MatrixTurn(Matrix, M, N, T):
-
-    listed_matrix = []
-
-    if M >= N: # define the amount of circuits
-        circuits = N // 2
-    else:
-        circuits = M // 2
-
-    for i in range(M):  # transform input matrix into the list
-        current_list = []
-        for j in range(N):
-            current_list.append(Matrix[i][j])
-        listed_matrix.append(current_list)
-
-    for i in range(T):
-        listed_matrix = rotation(listed_matrix, circuits)
-
-    Matrix = listed_matrix
+    for index, line in enumerate(matrix):
+        line = "".join(matrix[index])
+        data[index] = line
