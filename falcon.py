47f5b6cb204c4b4a4e92e3af0edbf88ef31e6a39
rmakarenko <roman.makarenko.spb@gmail.com>
2021-12-25 00:08:56 +0300

    Create falcon.py

---
+++ falcon.py
@@ -0,0 +1,31 @@
+def maximum(massive, start, finish):
+    max = massive[0]
+    for i in range(start, finish + 1):
+        if massive[i] > max:
+            max = massive[i]
+    return max
+
+def Transform(A, N):
+    B = []
+    for i in range(len(A) - 1):
+        for j in range(len(A) - 1 - i):
+            k = i + j
+            B.append(maximum(A, j, k))
+    return B
+
+def TransformTransform(A, N):
+
+    sum = 0
+
+    B = Transform(A, N)
+    B = Transform(B, N)
+
+    for i in range(len(B)):
+        sum = sum + B[i]
+
+    if sum % 2 == 0:
+        return True
+    else:
+        return False
+
+print(TransformTransform([1,2,3,4,5,6], 6))
