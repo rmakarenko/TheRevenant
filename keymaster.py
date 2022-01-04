f83802f35a12ed60732f8159f8246a991b0303ef
rmakarenko <roman.makarenko.spb@gmail.com>
2022-01-05 02:09:24 +0300

    Create keymaster.py

---
+++ keymaster.py
@@ -0,0 +1,19 @@
+def Keymaker(k):
+
+    doors = []
+    returner = ''
+
+    for i in range(k):
+        doors.append(0)
+
+    for j in range(k):
+        for i in range(0, len(doors), 1 + j):
+            if doors[i] == 1:
+                doors[i] = 0
+            else:
+                doors[i] = 1
+
+    for i in range(len(doors)):
+        returner = returner + str(doors[i])
+
+    return returner
